2394bdf2ee66a3615836553b63fff117278740ef
Herve Pages <hpages@fhcrc.org>
2015-03-04 22:44:37 +0000

    use readGAlignment*() instead of deprecated readGAlignment*FromBam() functions
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/trackViewer@100236 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/importBam.R
+++ R/importBam.R
@@ -3,16 +3,16 @@ importBam <- function(file, file2, ranges=GRanges(), pairs=FALSE){
         stop("ranges must be an object of GRanges.")
     param <- ScanBamParam(which=ranges)
     if(pairs){
-        bam <- readGAlignmentPairsFromBam(file, param=param)
+        bam <- readGAlignmentPairs(file, param=param)
     }else{
-        bam <- readGAlignmentsFromBam(file, param=param)
+        bam <- readGAlignments(file, param=param)
     }
     gr <- coverageGR(bam)
     if(!missing(file2)){
         if(pairs){
-            bam2 <- readGAlignmentPairsFromBam(file2, param=param)
+            bam2 <- readGAlignmentPairs(file2, param=param)
         }else{
-            bam2 <- readGAlignmentsFromBam(file2, param=param)
+            bam2 <- readGAlignments(file2, param=param)
         }
         gr2 <- coverageGR(bam2)
         return(new("track", dat=orderedGR(gr), dat2=orderedGR(gr2),
