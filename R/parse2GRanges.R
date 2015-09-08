b9161a5d71d2f928c7dc8ccd714c75b3c4c7933d
Jianhong Ou <jianhong.ou@umassmed.edu>
2015-09-08 17:12:19 +0000

    parse2GRanges function to parse text into GRanges
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/trackViewer@108286 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/parse2GRanges.R
@@ -0,0 +1,18 @@
+parse2GRanges <- function(text){
+    ## chr13:99,443,451-99,848,821:-
+    x <- gsub(",", "", text)
+    x <- do.call(rbind, strsplit(x, ":"))
+    if(ncol(x)==1){
+        stop("Can not handle the text.")
+    }
+    coor <- do.call(rbind, strsplit(x[,2], "\\-"))
+    if(ncol(coor)!=2){
+        stop("Can not handle the text.")
+    }
+    if(ncol(x)==2){
+        x <- cbind(x, "*")
+    }
+    GRanges(x[,1], 
+            IRanges(as.numeric(coor[,1]), as.numeric(coor[,2])),
+            strand=x[,3])
+}
