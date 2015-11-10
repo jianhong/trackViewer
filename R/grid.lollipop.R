bc2ceb75406ee4db55d809620cb34231169b530b
Jianhong Ou <jianhong.ou@umassmed.edu>
2015-11-10 22:57:01 +0000

    add lolliplot
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/trackViewer@110524 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/grid.lollipop.R
@@ -0,0 +1,144 @@
+grid.pie <- function (x=.5, y=.5, 
+                      radius=.8,
+                      col=NULL,
+                      border=NULL,
+                      percent=NULL,
+                      edges=100) {
+    if(length(percent)>0) percent <- unlist(percent[, sapply(percent, is.numeric)])
+    if(length(percent)<1){
+        percent <- 1
+    }
+    percent <- c(0, cumsum(percent)/sum(percent))
+    dx <- diff(percent)
+    nx <- length(dx)
+    if (is.null(col)) 
+        col <- c("white", "lightblue", "mistyrose", "lightcyan", 
+                 "lavender", "cornsilk")
+    if (!is.null(col)) 
+        col <- rep_len(col, nx)
+    if (!is.null(border)) 
+        border <- rep_len(border, nx)
+    twopi <- 2 * pi
+    ratio.yx <- 1/as.numeric(convertX(unit(1, "snpc"), "npc"))
+    t2xy <- function(t) {
+        t2p <- twopi * t + pi/2
+        list(x = radius * cos(t2p), y = radius * sin(t2p) * ratio.yx)
+    }
+    for (i in 1L:nx) {
+        n <- max(2, floor(edges * dx[i]))
+        P <- t2xy(seq.int(percent[i], percent[i + 1], length.out = n))
+        grid.polygon(unit(c(P$x, 0)+x,"npc"), unit(c(P$y, 0)+y, "npc"), gp=gpar(col = border[i], fill = col[i]))
+    }
+    invisible(NULL)
+}
+
+rgb2hex <- function(x){
+    hex <- function(a) format(as.hexmode(a), width=2, upper.case=TRUE)
+    paste0("#",hex(x[1]),hex(x[2]),hex(x[3]))
+}
+grid.lollipop <- function (x1=.5, y1=.5,
+                           x2=.5, y2=.75,
+                           y3=.04, y4=.02,
+                           radius=.8,
+                           col=NULL,
+                           border=NULL,
+                           percent=NULL,
+                           edges=100,
+                           type=c("circle", "pie", "pin"),
+                           ratio.yx=1,
+                           pin=NULL,
+                           scoreMax){
+    stopifnot(is.numeric(c(x1, x2, y1, y2, y3, y4, radius, edges)))
+    type <- match.arg(type)
+    if(type!="pie"){
+        this.score <- if(length(percent$score)>0) ceiling(percent$score) else 1
+        if(type=="circle"){
+            grid.lines(x=c(x1, x1, x2, x2), y=c(y1, y2, y2+y3, y2+y3+y4+scoreMax*ratio.yx), 
+                       gp=gpar(col=border))
+        }else{
+            grid.lines(x=c(x1, x1, x2, x2), y=c(y1, y2, y2+y3, y2+y3+y4+(this.score-.5)*2*radius*ratio.yx), 
+                       gp=gpar(col=border))
+        }
+        
+    }else{
+        grid.lines(x=c(x1, x1, x2, x2), y=c(y1, y2, y2+y3, y2+y3+y4), 
+                   gp=gpar(col=border))
+    }
+    
+    if(length(pin)>0){
+        if(length(border)>0) pin@paths[[2]]@rgb <- rgb2hex(col2rgb(border[1]))
+        if(length(col)>0) pin@paths[[1]]@rgb <- rgb2hex(col2rgb(col[1]))
+        if(length(col)>1) pin@paths[[3]]@rgb <- rgb2hex(col2rgb(col[2]))
+    }
+    switch(type,
+           circle={
+               for(i in 1:this.score){
+                   grid.circle(x=x2, y=y2+y3+y4/2+2*radius*ratio.yx*(i-.5),
+                               r=radius*ratio.yx, 
+                               gp=gpar(col=border, fill=col))
+               }
+               },
+           pie=grid.pie(x=x2, y=y2+y3+y4+radius, 
+                             radius = radius, 
+                             col = col, 
+                             border = border, 
+                             percent=percent,
+                             edges=edges),
+           pin=grid.picture(picture=pin, x=x2, 
+                            y=y2+y3+(this.score-.5)*2*radius*ratio.yx+y4/2,
+                            width=2*radius*ratio.yx,
+                            height=3*radius*ratio.yx+y4),
+           grid.pie(x=x2, y=y2+y3+y4+radius, 
+                    radius = radius, 
+                    col = col, 
+                    border = border, 
+                    percent=percent,
+                    edges=edges))
+}
+
+jitterLables <- function(coor, xscale, lineW, weight=1.2){
+    if(weight==1.2) stopifnot(order(coor)==1:length(coor))
+    if(weight<0.5) return(coor)
+    stopifnot(length(xscale)==2)
+    pos <- (coor-xscale[1])/diff(xscale)
+    pos.diff <- diff(c(0, pos, 1))
+    idx <- which(pos.diff < weight*lineW)
+    if(length(idx)<1){
+        return(coor)
+    }
+    if(all(idx %in% c(1, length(pos)))){
+        return(coor)
+    }
+    idx.diff <- diff(c(-1, idx))
+    idx.grp <- rle(idx.diff)
+    idx.grp$values[idx.grp$values==1] <- length(pos) + 1:sum(idx.grp$values==1)
+    idx.grp <- inverse.rle(idx.grp)
+    idx.grp[which(idx.grp>length(pos))-1] <- idx.grp[idx.grp>length(pos)]
+    idx.grp <- split(idx, idx.grp)
+    flag <- as.numeric(names(idx.grp))>length(pos)
+    idx.grp.mul <- lapply(idx.grp[flag], function(.ele){
+        c(.ele[1]-1, .ele)
+    })
+    idx.grp.sin <- lapply(idx.grp[!flag], function(.ele){
+        lapply(as.list(.ele), function(.ele){c(.ele-1, .ele)})
+    })
+    idx.grp.sin <- unlist(idx.grp.sin, recursive = FALSE)
+    idx.grp <- c(idx.grp.mul, idx.grp.sin)
+    
+    adj.pos <- lapply(idx.grp, function(.ele){
+        .ele <- .ele[.ele>0 & .ele<=length(pos)]
+        this.pos <- pos[.ele]
+        names(this.pos) <- .ele
+        if(length(this.pos)%%2==1){
+            center <- ceiling(length(this.pos)/2)
+        }else{
+            center <- length(this.pos)/2 + .5
+        }
+        this.pos <- this.pos + ((1:length(this.pos))-center) * weight * lineW
+    })
+    names(adj.pos) <- NULL
+    adj.pos <- unlist(adj.pos)
+    coor[as.numeric(names(adj.pos))] <- adj.pos*diff(xscale)+xscale[1]
+    
+    Recall(coor, xscale=xscale, lineW=lineW, weight=weight-0.2)
+}
---
+++ R/lollipop.plot.R
@@ -0,0 +1,155 @@
+lolliplot <- function(SNP.gr, features=NULL, 
+                          type=c("circle", "pie", "pin"),
+                          newpage=TRUE){
+    stopifnot(inherits(SNP.gr, c("GRanges", "GRangesList")))
+    stopifnot(inherits(features, c("GRanges", "GRangesList")))
+    type <- match.arg(type)
+    if(type=="pin"){
+        pinpath <- system.file("extdata", "map-pin-red.xml", package="trackViewer")
+        pin <- readPicture(pinpath)
+    }else{
+        pin <- NULL
+    }
+    SNP.gr.name <- deparse(substitute(SNP.gr))
+    if(class(SNP.gr)=="GRanges"){
+        SNP.gr <- GRangesList(SNP.gr)
+        names(SNP.gr) <- SNP.gr.name
+    }
+    len <- length(SNP.gr)
+    features.name <- deparse(substitute(features))
+    if(class(features)=="GRanges"){
+        ranges <- range(features)[rep(1, len)]
+    }else{
+        if(length(features)!=len){
+            stop("if both SNP.gr and features is GRangesList,",
+                 " the lengthes of them should be identical.")
+        }
+        ranges <- unlist(GRangesList(lapply(features, range)))
+    }
+    if(class(ranges)=="GRanges"){
+        ##cut all SNP.gr by the range
+        for(i in len){
+            range <- ranges[i]
+            stopifnot(all(width(SNP.gr[[i]])==1))
+            ol <- findOverlaps(SNP.gr[[i]], range)
+            SNP.gr[[i]] <- SNP.gr[[i]][queryHits(ol)]
+        }
+    }
+    height <- 1/len
+    if(newpage) grid.newpage()
+    for(i in 1:len){
+        vp <- viewport(x=.5, y=height*(i-0.5), width=1, height=height)
+        pushViewport(vp)
+        lineW <- as.numeric(convertX(unit(1, "line"), "npc"))
+        lineH <- as.numeric(convertY(unit(1, "line"), "npc"))
+        ## ylab
+        if(length(names(SNP.gr))>0){
+            grid.text(names(SNP.gr)[i], x = 2 * lineW, 
+                      y = .5, rot = 90)
+        }
+        
+        if(class(features)=="GRangesList"){
+            feature <- features[[i]]
+        }else{
+            feature <- features
+        }
+        baseline <- max(c(unlist(feature$height)/2, .0001)) + 0.2 * lineH
+        gap <- .2 * lineH
+        bottomblank <- 4
+        if(length(names(feature))>0){
+            sW <- max(as.numeric(convertX(stringWidth(names(feature)), "line"))) + 1
+            ncol <- floor(as.numeric(convertX(unit(1, "npc"), "line")) / 
+                              sW / as.numeric(convertX(stringWidth("W"), "line")))
+            bottomblank <- max(ceiling(length(names(feature)) / ncol), 4)
+            pushViewport(viewport(x=.5, y=bottomblank*lineH/2, 
+                                  width=1,
+                                  height=bottomblank*lineH,
+                                  xscale=c(start(ranges[i]), end(ranges[i]))))
+            color <- if(length(unlist(feature$color))==length(feature)) 
+                unlist(feature$color) else "black"
+            fill <- if(length(unlist(feature$fill))==length(feature)) 
+                unlist(feature$fill) else "black"
+            pch <- if(length(unlist(feature$pch))==length(feature)) 
+                unlist(feature$pch) else 22
+            grid.legend(label=names(feature), ncol=ncol,
+                      byrow=TRUE, vgap=unit(.2, "lines"),
+                      pch=pch,
+                      gp=gpar(col=color, fill=fill))
+            popViewport()
+        }
+        pushViewport(viewport(x=lineW + .5, y= (bottomblank+2)*lineH/2 + .5, 
+                              width= 1 - 6*lineW,
+                              height= 1 - (bottomblank+2)*lineH,
+                              xscale=c(start(ranges[i]), end(ranges[i]))))
+        ## axis
+        grid.xaxis()
+        
+        ##baseline
+        grid.lines(x=c(0, 1), y=c(baseline, baseline)) #baseline
+        for(m in 1:length(feature)){
+            this.dat <- feature[m]
+            color <- if(is.list(this.dat$color)) this.dat$color[[1]] else this.dat$color
+            fill <- if(is.list(this.dat$fill)) this.dat$fill[[1]] else this.dat$fill
+            this.cex <- if(length(this.dat$cex)>0) this.dat$cex[[1]][1] else 1
+            width <- if(length(this.dat$height)>0) this.dat$height[[1]][1] else 2*baseline
+            rot <- if(length(this.dat$rot)>0) this.dat$rot[[1]][1] else 45
+            grid.rect(x=start(this.dat), y=baseline, width=width(this.dat), height=width,
+                      just="left", gp=gpar(col=color, fill=fill), default.units = "native")
+        }
+        SNPs <- SNP.gr[[i]]
+        strand(SNPs) <- "*"
+        SNPs <- sort(SNPs)
+        width <- 2 * baseline + 2*gap
+        lab.pos <- jitterLables(start(SNPs), 
+                                xscale=c(start(ranges[i]), end(ranges[i])), 
+                                lineW=lineW)
+        if(length(SNPs)>0){
+            scoreMax <- if(length(SNPs$score)>0) ceiling(max(c(SNPs$score, 1), na.rm=TRUE)) else 1
+            ratio.yx <- 1/as.numeric(convertX(unit(1, "snpc"), "npc"))
+            for(m in 1:length(SNPs)){
+                this.dat <- SNPs[m]
+                color <- if(is.list(this.dat$color)) this.dat$color[[1]] else this.dat$color
+                border <- if(is.list(this.dat$border)) this.dat$border[[1]] else this.dat$border
+                fill <- if(is.list(this.dat$fill)) this.dat$fill[[1]] else this.dat$fill
+                grid.lollipop(x1=(start(this.dat)-start(ranges[i]))/width(ranges[i]), 
+                              y1=baseline,
+                              x2=(lab.pos[m]-start(ranges[i]))/width(ranges[i]), y2=width,
+                              y3=4*gap, y4=2.5*gap, 
+                              radius=lineW/2,
+                              col=color,
+                              border=border,
+                              percent=mcols(this.dat),
+                              edges=100,
+                              type=type,
+                              ratio.yx=ratio.yx,
+                              pin=pin,
+                              scoreMax=(scoreMax-0.5) * lineW)
+            }
+            if(length(names(SNPs))>0){
+                switch(type,
+                       circle={
+                           grid.text(x=lab.pos, 
+                                     y=width + lineW*max(ratio.yx, 1.2) + 6.5*gap + (scoreMax-0.5) * lineW * ratio.yx, 
+                                     label = names(SNPs), rot=90, just="left", 
+                                     default.units = "native")
+                       },
+                       pin={
+                           this.scores <- if(length(SNPs$score)>0) ceiling(SNPs$score) else .5
+                           this.scores[is.na(this.scores)] <- .5
+                           grid.text(x=lab.pos, 
+                                     y=width + lineW*max(ratio.yx, 1.2) + 6.5*gap + (this.scores-0.5) * lineW * ratio.yx, 
+                                     label = names(SNPs), rot=90, just="left", 
+                                     default.units = "native")
+                       },
+                       pie={
+                           grid.text(x=lab.pos, y=width + lineW*max(ratio.yx, 1.2) + 6.5*gap, 
+                                     label = names(SNPs), rot=90, just="left", 
+                                     default.units = "native")
+                       })
+            }
+        }
+        
+        popViewport()
+        popViewport()
+    }
+}
