8748a0026fbd4e9955a53334800faadc4107fd20
Jianhong Ou <jianhongou@Jianhongs-MBP.dhe.duke.edu>
2019-03-04 18:54:46 -0500

    Build shiny output objects for browseTracks

--- R/browseTracks.R
+++ R/browseTracks.R
@@ -316,3 +316,34 @@ browseTracks <- function(trackList,
         package = getPackageName()
     )
 }
+
+
+#' Shiny bindings for browseTracks
+#'
+#' Output and render functions for using browseTracks within Shiny
+#' applications and interactive Rmd documents.
+#'
+#' @param outputId output variable to read from
+#' @param width,height Must be a valid CSS unit (like \code{'100\%'},
+#'   \code{'400px'}, \code{'auto'}) or a number, which will be coerced to a
+#'   string and have \code{'px'} appended.
+#' @param expr An expression that generates a browseTracks
+#' @param env The environment in which to evaluate \code{expr}.
+#' @param quoted Is \code{expr} a quoted expression (with \code{quote()})? This
+#'   is useful if you want to save an expression in a variable.
+#'
+#' @name browseTracks-shiny
+#'
+#' @export
+browseTracksOutput <- function(outputId, width = '100%', height = '600px'){
+  htmlwidgets::shinyWidgetOutput(outputId, 'browseTracks', width, height, 
+                                 package = 'trackViewer')
+}
+
+#' @rdname browseTracks-shiny
+#' @export
+renderbrowseTracks <- function(expr, env = parent.frame(), quoted = FALSE) {
+  if (!quoted) { expr <- substitute(expr) } # force quoted
+  htmlwidgets::shinyRenderWidget(expr, browseTracksOutput, env, quoted = TRUE)
+}
+
