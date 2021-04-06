d6d2b3211a52bdaa7c9e2dd76b2fc02d2e20fd42
jianhong <jianhong.ou@gmail.com>
2021-04-06 12:58:08 -0400

    clean staging branch

--- inst/unitTests/test_coverage.R
+++
@@ -1,7 +0,0 @@
-test_coverageGR<-function(){
-	gr <- GRanges("chr1", IRanges(c(1:10), width=3), strand="+", score=rep(1, 10))
-    gr <- coverageGR(gr)
-	checkEquals(c(1, 2, 3, 2, 1), score(gr))
-	checkEquals(c(1, 2, 3, 11, 12), start(gr))
-	checkEquals(c(1, 2, 10, 11, 12), end(gr))
-}
--- tests/runTests.R
+++
@@ -1,3 +0,0 @@
-require("trackViewer") || stop("unable to load Package:trackViewer")
-require("RUnit") || stop("unable to load Package:RUnit")
-BiocGenerics:::testPackage("trackViewer")
